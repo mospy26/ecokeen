120b3666f2de04bb0132ca9d1f7589ac882c3fe8
Brendon Lam <brendonlam123@hotmail.com>
2020-08-28 20:15:57 +1000

    Initialized backend files

---
+++ backend/src/test/java/com/ecokeen/backend/EcokeenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecokeen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcokeenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
