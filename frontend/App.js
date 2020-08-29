df608f05ad6291e61b12a00c84291e979d005913
Shrawani Bhattarai <sbha8524@uni.sydney.edu.au>
2020-08-29 14:40:16 +1000

    Few changes

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from  'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Home from './src/screens/Home';
 import TravelInput from './src/screens/TravelInput';
 import GroceryInput from './src/screens/GroceryInput';
+import History from './src/screens/History';
 
 
 const navigator = createStackNavigator({
-  Home: Home,
-  TravelInput: TravelInput,
-  GroceryInput: GroceryInput,
+    Home: Home,
+    TravelInput: TravelInput,
+    GroceryInput: GroceryInput,
+    History: History,
 
 }, {
-initialRouteName: "Home",
-defaultNavigationOptions:{
-  title: "Ecokeen"
-}
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+        title: "Ecokeen"
+    }
 })
 
 export default createAppContainer(navigator)
