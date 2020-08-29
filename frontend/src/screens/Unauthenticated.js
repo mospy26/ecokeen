5195a578d261000bf83acea616e680ab0406df4e
mospy26 <m.mustafaahmed@hotmail.com>
2020-08-29 15:43:37 +1000

    Email validation added

--- frontend/src/screens/Unauthenticated.js
+++ frontend/src/screens/Unauthenticated.js
@@ -11,6 +11,12 @@ export default class Unauthenticated extends Component {
         }
     }
 
+    validate = (email) => {
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    
+        return expression.test(String(email).toLowerCase())
+    }
+
     changeEmail = (email) => {
         this.setState({email: email});
     }
@@ -20,7 +26,7 @@ export default class Unauthenticated extends Component {
     }
 
     enableLogin = () => {
-        return this.state.email.length > 0 && this.state.password.length > 0
+        return this.state.email.length > 0 && this.state.password.length > 0 && this.validate(this.state.email)
     }
 
     authenticate() {
