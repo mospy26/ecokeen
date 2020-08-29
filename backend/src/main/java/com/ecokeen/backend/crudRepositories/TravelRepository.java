99b5c978309d5c57921e639b9fd2391c07dd1f7f
Brendon Lam <brendonlam123@hotmail.com>
2020-08-30 00:29:21 +1000

    Added JPA to TravelRepo

--- backend/src/main/java/com/ecokeen/backend/crudRepositories/TravelRepository.java
+++ backend/src/main/java/com/ecokeen/backend/crudRepositories/TravelRepository.java
@@ -2,10 +2,11 @@ package com.ecokeen.backend.crudRepositories;
 
 import com.ecokeen.backend.model.Travel;
 import com.ecokeen.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface TravelRepository extends CrudRepository<Travel, Integer> {
+public interface TravelRepository extends JpaRepository<Travel, Integer> {
 }
