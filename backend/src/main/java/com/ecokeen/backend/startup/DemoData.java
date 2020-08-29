94e24ff0a92aaa715c737adab8c9dddbc0cde16a
Brendon Lam <brendonlam123@hotmail.com>
2020-08-30 00:25:41 +1000

    Demo Data

--- backend/src/main/java/com/ecokeen/backend/startup/DemoData.java
+++ backend/src/main/java/com/ecokeen/backend/startup/DemoData.java
@@ -42,120 +42,34 @@ public class DemoData {
 
     @EventListener
     public void appReady(ApplicationReadyEvent event) {
-        int numUsers = 10;
+//        deleteAll();
+//        onlyUsers();
+        withRelationship();
+    }
 
-//        Intialize Users
-//        ArrayList<User> users = new ArrayList<User>();
-//        for (int i = 0; i < numUsers; i++) {
-//            User u = new User();
-//            u.setName(String.format("User %d", i));
-//            u.setPassword("password");
-//            u.setEmail(String.format("user%d@gmail.com", i));
-//            users.add(u);
-//        }
-//        for (User u : users) {
-//            userRepository.save(u);
-//        }
-//
-//        // Initialize Grocery Types
-//        int numGroceryType = 5;
-//        ArrayList<GroceryType> gtList = new ArrayList<>();
-//        for (int i = 0; i < numGroceryType; i++) {
-//            gtList.add(new GroceryType());
-//        }
-//        gtList.get(0).setName("Chips");
-//        gtList.get(1).setName("Meat");
-//        gtList.get(2).setName("Milk");
-//        gtList.get(3).setName("Toiletries");
-//        gtList.get(4).setName("Cleaning Supplies");
-//        for (GroceryType gt : gtList) {
-//            groceryTypeRepository.save(gt);
-//        }
-//
-//        // Initialize Grocery Brand
-//        int numGroceryBrand = 3;
-//        ArrayList<GroceryBrand> gbList = new ArrayList<>();
-//        for (int i = 0; i < numGroceryBrand; i++) {
-//            gbList.add(new GroceryBrand());
-//        }
-//        gbList.get(0).setName("Coles");
-//        gbList.get(1).setName("Woolworths");
-//        gbList.get(2).setName("Aldi");
-//        for (GroceryBrand gb : gbList) {
-//            groceryBrandRepository.save(gb);
-//        }
-//
-//        // Initialize Travel
-//        int numTravel = 5;
-//        ArrayList<Travel> travelList = new ArrayList<>();
-//        for (int i = 0; i < numTravel; i++) {
-//            Travel t = new Travel();
-//            t.setTimestamp(new Timestamp(new Date().getTime()));
-//            double fd = i + 1 + 0.5;
-//            t.setFootprint((float) fd);
-//            t.setDistance((float) fd);
-//            travelList.add(t);
-//        }
-//        for (Travel t : travelList) {
-//            travelRepository.save(t);
-//        }
-//
-//        // Initialize Groceries
-//        int numGroceries = 5;
-//        ArrayList<Grocery> groceryList = new ArrayList<>();
-//        for (int i = 0; i < numGroceries; i++) {
-//            Grocery g = new Grocery();
-//            double fd = i + 1 + 0.5;
-//            g.setFootprint((float) fd);
-//            g.setQuantity(i+1);
-//            g.setTimestamp(new Timestamp(new Date().getTime()));
-//            groceryList.add(g);
-//        }
-//        for (Grocery g : groceryList) {
-//            groceryRepository.save(g);
-//        }
-//
-//        // Initialize Vehicle Type
-//        int numVehicleType = 2;
-//        ArrayList<VehicleType> vehicleTypesList = new ArrayList<>();
-//        for (int i = 0; i < numVehicleType; i++) {
-//            VehicleType vt = new VehicleType();
-//            vehicleTypesList.add(vt);
-//        }
-//        vehicleTypesList.get(0).setName("Car");
-//        vehicleTypesList.get(1).setName("Train");
-//        for (VehicleType vt : vehicleTypesList) {
-//            vehicleTypeRepository.save(vt);
-//        }
-//
-//        // Initialize Vehicle Brand
-//        int numVehicleBrand = 2;
-//        ArrayList<VehicleBrand> vehicleBrandList = new ArrayList<>();
-//        for (int i = 0; i < numVehicleBrand ; i++) {
-//            VehicleBrand vb = new VehicleBrand();
-//            vehicleBrandList.add(vb);
-//        }
-//        vehicleBrandList.get(0).setName("Toyota");
-//        vehicleBrandList.get(1).setName("Mitsubishi");
-//        for (VehicleBrand vb : vehicleBrandList) {
-//            vehicleBrandRepository.save(vb);
-//        }
-//
-//        // Initialize Fuel
-//        int numFuel = 3;
-//        ArrayList<Fuel> fuelList = new ArrayList<>();
-//        for (int i = 0; i < numFuel; i++) {
-//            Fuel f = new Fuel();
-//            double cv = i + 0.5;
-//            f.setCarbonValue((float) cv);
-//            fuelList.add(f);
-//        }
-//        fuelList.get(0).setType("Unleaded 91");
-//        fuelList.get(1).setType("Unleaded 98");
-//        fuelList.get(2).setType("Petrol");
-//        for (Fuel f : fuelList) {
-//            fuelRepository.save(f);
-//        }
+    public void deleteAll() {
+        userRepository.deleteAll(userRepository.findAll());
+        groceryBrandRepository.deleteAll(groceryBrandRepository.findAll());
+        groceryRepository.deleteAll(groceryRepository.findAll());
+        groceryTypeRepository.deleteAll(groceryTypeRepository.findAll());
+        travelRepository.deleteAll(travelRepository.findAll());
+        vehicleBrandRepository.deleteAll(vehicleBrandRepository.findAll());
+        vehicleTypeRepository.deleteAll(vehicleTypeRepository.findAll());
+        fuelRepository.deleteAll(fuelRepository.findAll());
+    }
+
+    public void onlyUsers() {
+        for (int i = 0; i < 10; i++) {
+            User u = new User();
+            u.setName("User " + i);
+            u.setEmail(String.format("User%d@gmail.com", i));
+            u.setPassword("password");
+            userRepository.save(u);
+        }
+    }
+
+    public void withRelationship() {
+        int numUsers = 10;
 
         /*
         FIELDS:
@@ -234,7 +148,122 @@ public class DemoData {
             fuelRepository.save(f);
             travelRepository.save(t);
         }
-
     }
 
-}
+    public void noRelationshipInitialization() {
+        int numUsers = 10;
+        // Intialize Users
+            ArrayList<User> users = new ArrayList<User>();
+            for (int i = 0; i < numUsers; i++) {
+                User u = new User();
+                u.setName(String.format("User %d", i));
+                u.setPassword("password");
+                u.setEmail(String.format("user%d@gmail.com", i));
+                users.add(u);
+            }
+            for (User u : users) {
+                userRepository.save(u);
+            }
+
+            // Initialize Grocery Types
+            int numGroceryType = 5;
+            ArrayList<GroceryType> gtList = new ArrayList<>();
+            for (int i = 0; i < numGroceryType; i++) {
+                gtList.add(new GroceryType());
+            }
+            gtList.get(0).setName("Chips");
+            gtList.get(1).setName("Meat");
+            gtList.get(2).setName("Milk");
+            gtList.get(3).setName("Toiletries");
+            gtList.get(4).setName("Cleaning Supplies");
+            for (GroceryType gt : gtList) {
+                groceryTypeRepository.save(gt);
+            }
+
+            // Initialize Grocery Brand
+            int numGroceryBrand = 3;
+            ArrayList<GroceryBrand> gbList = new ArrayList<>();
+            for (int i = 0; i < numGroceryBrand; i++) {
+                gbList.add(new GroceryBrand());
+            }
+            gbList.get(0).setName("Coles");
+            gbList.get(1).setName("Woolworths");
+            gbList.get(2).setName("Aldi");
+            for (GroceryBrand gb : gbList) {
+                groceryBrandRepository.save(gb);
+            }
+
+            // Initialize Travel
+            int numTravel = 5;
+            ArrayList<Travel> travelList = new ArrayList<>();
+            for (int i = 0; i < numTravel; i++) {
+                Travel t = new Travel();
+                t.setTimestamp(new Timestamp(new Date().getTime()));
+                double fd = i + 1 + 0.5;
+                t.setFootprint((float) fd);
+                t.setDistance((float) fd);
+                travelList.add(t);
+            }
+            for (Travel t : travelList) {
+                travelRepository.save(t);
+            }
+
+            // Initialize Groceries
+            int numGroceries = 5;
+            ArrayList<Grocery> groceryList = new ArrayList<>();
+            for (int i = 0; i < numGroceries; i++) {
+                Grocery g = new Grocery();
+                double fd = i + 1 + 0.5;
+                g.setFootprint((float) fd);
+                g.setQuantity(i+1);
+                g.setTimestamp(new Timestamp(new Date().getTime()));
+                groceryList.add(g);
+            }
+            for (Grocery g : groceryList) {
+                groceryRepository.save(g);
+            }
+
+            // Initialize Vehicle Type
+            int numVehicleType = 2;
+            ArrayList<VehicleType> vehicleTypesList = new ArrayList<>();
+            for (int i = 0; i < numVehicleType; i++) {
+                VehicleType vt = new VehicleType();
+                vehicleTypesList.add(vt);
+            }
+            vehicleTypesList.get(0).setName("Car");
+            vehicleTypesList.get(1).setName("Train");
+            for (VehicleType vt : vehicleTypesList) {
+                vehicleTypeRepository.save(vt);
+            }
+
+            // Initialize Vehicle Brand
+            int numVehicleBrand = 2;
+            ArrayList<VehicleBrand> vehicleBrandList = new ArrayList<>();
+            for (int i = 0; i < numVehicleBrand ; i++) {
+                VehicleBrand vb = new VehicleBrand();
+                vehicleBrandList.add(vb);
+            }
+            vehicleBrandList.get(0).setName("Toyota");
+            vehicleBrandList.get(1).setName("Mitsubishi");
+            for (VehicleBrand vb : vehicleBrandList) {
+                vehicleBrandRepository.save(vb);
+            }
+
+            // Initialize Fuel
+            int numFuel = 3;
+            ArrayList<Fuel> fuelList = new ArrayList<>();
+            for (int i = 0; i < numFuel; i++) {
+                Fuel f = new Fuel();
+                double cv = i + 0.5;
+                f.setCarbonValue((float) cv);
+                fuelList.add(f);
+            }
+            fuelList.get(0).setType("Unleaded 91");
+            fuelList.get(1).setType("Unleaded 98");
+            fuelList.get(2).setType("Petrol");
+            for (Fuel f : fuelList) {
+                fuelRepository.save(f);
+            }
+        }
+
+    }
