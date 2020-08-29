68360b19b16bb21eac9e66fa636526a94392316f
Chris Pidd <chrispidd@vlan-2662-10-19-252-89.student.wireless.sydney.edu.au>
2020-08-29 15:50:18 +1000

    Added tab to home screena and buttons to incomplete histories #1

--- frontend/App.js
+++ frontend/App.js
@@ -6,13 +6,19 @@ import {createStackNavigator} from  'react-navigation-stack';
 import Home from './src/screens/Home';
 import TravelInput from './src/screens/TravelInput';
 import GroceryInput from './src/screens/GroceryInput';
-
+import MainHistory from './src/screens/MainHistory';
+import GroceryHistory from './src/screens/GroceryHistory';
+import TravelHistory from '././src/screens/TravelHistory';
+import Profile from './src/screens/Profile'
 
 const navigator = createStackNavigator({
   Home: Home,
   TravelInput: TravelInput,
   GroceryInput: GroceryInput,
-
+  MainHistory: MainHistory,
+  GroceryHistory: GroceryHistory,
+  TravelHistory: TravelHistory,
+  Profile: Profile,
 }, {
 initialRouteName: "Home",
 defaultNavigationOptions:{
--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
-import { Text, View, Button, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Icon } from 'react-native-elements'
 
-export default class Home extends Component {
+export default class Home extends Component {  
     render() {
+        const styles = StyleSheet.create({
+            tabNavigationStyle: {
+                position: 'absolute',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                width: '100%', 
+                height: 50, 
+                bottom: 15,
+
+            },
+            tabIconStyle:{
+                
+            }
+          });
         return (
+            
             <View style={{backgroundColor:'white', flex:1}}>
                 <Text style={{marginLeft: 20, marginTop: 40, color: 'black', fontSize: 15}}> WELCOME </Text>
                 <Text style={{marginLeft: 20, color: 'black', fontSize: 70, fontWeight: 'bold', color:'#284142'}}> User </Text>
@@ -16,6 +32,28 @@ export default class Home extends Component {
                     <Text style={{fontSize: 18, color: "#fff", fontWeight: "bold", alignSelf: "center",textTransform: "uppercase"}}>to travel input</Text>
                 </TouchableOpacity>
 
+                <TouchableOpacity onPress={()=>{this.props.navigation.navigate("GroceryHistory")}} style={{elevation: 8, backgroundColor: "#284243", borderRadius: 5, paddingVertical: 20, paddingHorizontal: 12, marginTop: 20, marginLeft: 20, marginRight:20}}>
+                    <Text style={{fontSize: 18, color: "#fff", fontWeight: "bold", alignSelf: "center",textTransform: "uppercase"}}>to grocery history</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>{this.props.navigation.navigate("TravelHistory")}} style={{elevation: 8, backgroundColor: "#284243", borderRadius: 5, paddingVertical: 20, paddingHorizontal: 12, marginTop: 20, marginLeft: 20, marginRight:20}}>
+                    <Text style={{fontSize: 18, color: "#fff", fontWeight: "bold", alignSelf: "center",textTransform: "uppercase"}}>to travel history</Text>
+                </TouchableOpacity>
+
+                <View style={styles.tabNavigationStyle}>
+                    <TouchableOpacity onPress={() => {this.props.navigation.navigate('Home');}}>
+                        <Icon reverse name="home"> </Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {this.props.navigation.navigate('GroceryHistory');}}>
+                        <Icon reverse name="shopping-cart"> </Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {this.props.navigation.navigate('TravelHistory');}}>
+                        <Icon reverse name="drive-eta"> </Icon>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {this.props.navigation.navigate('Profile');}}>
+                    <   Icon reverse name="person"> </Icon>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
